4ee63397e34a1e235ab3ed707389f12372159077
NeserCode <73767966+NeserCode@users.noreply.github.com>
2022-05-16 18:34:21 +0800

    update
    快点黑块

--- Vanilla JS/Don't touch white block/js/vanilla.js
+++ Vanilla JS/Don't touch white block/js/vanilla.js
@@ -8,6 +8,10 @@ var clickArr = []   // 可否点击队列
     , score = 0
     , isStarted = false
     , isPause = false
+    , stepCounter = {
+        on: 'left',
+        step: 1
+    }
     ;
 //方法定义
 function getScore() {
@@ -42,6 +46,7 @@ function getScore() {
     if (isStarted && !isPause) {
         isPause = true
         sBtn.disabled = false
+        clearInterval(timer[1])
 
         sBtn.innerHTML = "结束游戏"
         pBtn.innerHTML = "继续"
@@ -50,6 +55,11 @@ function getScore() {
         isPause = false
         sBtn.disabled = true
         mode.disabled = true
+
+        //恢复游戏模式运动轨迹
+        timer[1] = setInterval(() => {
+            Lantain()
+        }, 1000)
 
         sBtn.innerHTML = "游戏进行中"
         pBtn.innerHTML = "暂停"
@@ -63,7 +73,14 @@ function getScore() {
         sBtn.disabled = true
         pBtn.disabled = false
         mode.disabled = true
-    } else
+
+        //初始化游戏模式运动轨迹
+        initLantain()
+        timer[1] = setInterval(() => {
+            Lantain()
+        }, 1000)
+    }
+    else
         if (isStarted && isPause) {
             // 回到游戏开始
             isStarted = false
@@ -71,11 +88,14 @@ function getScore() {
             pBtn.disabled = true
             mode.disabled = false
 
+            clearInterval(timer[1])
+
             isPause = false
             pBtn.innerHTML = "暂停"
             sBtn.innerHTML = "开始游戏"
         }
 
+    //初始化分数
     score = 0
     showScore()
 }
@@ -85,11 +105,70 @@ function getScore() {
         sBtn.disabled = false
         pBtn.disabled = true
         mode.disabled = false
+        clearInterval(timer[1])
 
         sBtn.innerHTML = "开始游戏"
         pBtn.innerHTML = "暂停"
         scoreBody.innerHTML = `游戏结束, 得分 ${score}`
     }
+}
+; function Lantain() {
+    const { step, on } = stepCounter
+    if (on == 'left' && step < 5) {
+        console.log('left', step)
+        g.style.transform = `translate(${step * 100}%,0)`
+        stepCounter.step++
+        //do something
+        if (on == 'left' && step == 4) {
+            console.log('down corner')
+            stepCounter.on = 'down'
+            stepCounter.step = 1
+            //do something
+        }
+    }
+    else if (on == 'down' && step < 5) {
+        console.log('down', step)
+        g.style.transform = `translate(400%, ${step * 100}%)`
+        stepCounter.step++
+        //do something
+        if (on == 'down' && step == 4) {
+            console.log('right corner')
+            stepCounter.on = 'right'
+            stepCounter.step = 1
+            //do something
+        }
+    }
+    else if (on == 'right' && step < 5) {
+        console.log('right', step)
+        g.style.transform = `translate(${(4 - step) * 100}%, 400%)`
+        stepCounter.step++
+        //do something
+        if (on == 'right' && step == 4) {
+            console.log('up corner')
+            g.style.transform = `translate(0, 400%)`
+            stepCounter.on = 'up'
+            stepCounter.step = 1
+            //do something
+        }
+
+    }
+    else if (on == 'up' && step < 5) {
+        console.log('up', step)
+        g.style.transform = `translate(0, ${(4 - step) * 100}%)`
+        stepCounter.step++
+        //do something
+        if (on == 'up' && step == 4) {
+            console.log('left corner')
+            stepCounter.on = 'left'
+            stepCounter.step = 1
+            //do something
+        }
+
+    }
+}
+; function initLantain() {
+    stepCounter.on = "left"
+    stepCounter.step = 1
 }
 
 // init 初始化
